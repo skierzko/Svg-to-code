3c9331ef1f275830713cf2e0c63d8eee7ca952b8
Sylwester Kierzkowski <skierzko@gmail.com>
2026-03-03 18:12:50 +0100

    Added description and footer to main page

--- app/src/app/features/main/MainPage.tsx
+++ app/src/app/features/main/MainPage.tsx
@@ -12,6 +12,7 @@ export default function MainPage() {
   const [svgCode, setSvgCode] = useState<string>("");
   const [svgNodes, setSvgNodes] = useState<Node | SVGSVGElement | null>(null);
   const [pathNodes, setPathNodes] = useState<SVGPathElement[] | null>(null);
+  const [showDescription, setShowDescription] = useState<boolean>(false);
 
   useEffect(() => {
     if (!files || files.length === 0) {
@@ -54,11 +55,38 @@ export default function MainPage() {
 
   return (
     <>
-      <section className="text-center my-8">
+      <section className="text-center mt-8 mb-2">
         <h1 className="text-3xl">Convert SVG to Code</h1>
-        <p className="text-lg text-gray-700">
+        <p className="p-2 text-lg text-gray-700">
           Upload an SVG file to generate corresponding code
         </p>
+      </section>
+
+      <section className="px-6 space-y-2 text-sm text-gray-400">
+        <p>
+          This web-based tool enables fast and convenient conversion of SVG files into
+          HTML code. Simply select a file from your disk or drag and drop it into the
+          designated area to instantly generate the corresponding code.
+        </p>
+
+        {showDescription === false && (
+          <div
+            className="inline-block cursor-pointer text-black hover:text-blue-500"
+            onClick={() => setShowDescription(true)}
+          >
+            Show more
+          </div>
+        )}
+
+        {showDescription && (
+          <p>
+            The solution is designed for designers and developers who need full control
+            over the structure and attributes of SVG elements. In addition to conversion,
+            it allows real-time editing of existing tag attributes. Every change is
+            immediately reflected in both the preview and the generated HTML code,
+            significantly streamlining the optimization and customization process.
+          </p>
+        )}
       </section>
 
       <div className="sm:flex gap-4 m-4">
@@ -77,6 +105,61 @@ export default function MainPage() {
       <ShowCode svgCode={svgCode} />
 
       {svgCode && <ViewSvg svgCode={svgCode} isSticky={true} />}
+
+      <section className="p-6 py-20 border-t-2 bg-slate-900 text-white">
+        <h2 className="mb-8 text-3xl">About the Project</h2>
+        <p className="mb-2">
+          This project was created as a personal initiative to improve my skills in
+          building modern web applications using <strong>Next.js</strong>,{" "}
+          <strong>React</strong>, <strong>Tailwind</strong> and <strong>Docker</strong>.
+        </p>
+        <p className="mb-2">
+          Its main goal is to deepen practical knowledge of frontend architecture,
+          component-based development, state management, and real-time data handling in
+          React, while also gaining hands-on experience with server-side rendering and
+          application structure in Next.js.
+        </p>
+        <p className="mb-2">
+          Additionally, the project serves as an opportunity to strengthen my
+          understanding of containerization and deployment workflows using Docker,
+          including environment configuration and application packaging.
+        </p>
+        <p className="mb-6">
+          Beyond its practical functionality, this project represents a continuous
+          learning process and a step forward in mastering modern web development tools
+          and best practices.
+        </p>
+        <p className="mb-2">
+          View the source code on GitHub:
+          <a
+            href="https://github.com/skierzko/Svg-to-code"
+            target="_blank"
+            className="text-blue-600 hover:underline ml-2"
+          >
+            GitHub Repository
+          </a>
+        </p>
+        <p className="mb-2">
+          Go to the author's portfolio:
+          <a
+            href="https://kierzkowski.net"
+            target="_blank"
+            className="text-blue-600 hover:underline ml-2"
+          >
+            Portfolio
+          </a>
+        </p>
+      </section>
+
+      <section>
+        <div className="p-6 py-10 bg-gray-800 text-white text-center">
+          <p>© 2026 Sylwester Kierzkowski.</p>
+          <p>
+            Designed and developed by Sylwester Kierzkowski. This project is open for
+            copying, modification, and use for any purpose.
+          </p>
+        </div>
+      </section>
     </>
   );
 }
