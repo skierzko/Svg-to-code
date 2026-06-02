6cf252e7222f0eec3e4fcd465dc2351290218af6
Sylwester <110302952+skierzko@users.noreply.github.com>
2026-06-02 20:57:38 +0200

    Add NODE_ENV to ecosystem.config.js

--- app/ecosystem.config.js
+++ app/ecosystem.config.js
@@ -5,8 +5,14 @@ module.exports = {
       script: "npm",
       args: "start",
       env: {
+        NODE_ENV: "production",
         PORT: 3000,
       },
+      instances: 1,
+      exec_mode: "fork",
+      autorestart: true,
+      max_restarts: 10,
+      exp_backoff_restart_delay: 100
     },
   ],
 };
