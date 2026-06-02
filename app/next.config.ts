f8c086e952035a58d63860cf1b57298efdbab8b7
Sylwester <110302952+skierzko@users.noreply.github.com>
2026-06-02 21:01:07 +0200

    Enhance Next.js config with new options
    Added various configuration options for optimization and image handling.

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  compress: true,
+  productionBrowserSourceMaps: false,
+  poweredByHeader: false,
+  generateEtags: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60 * 60 * 24 * 30,
+  },
+  experimental: {
+    optimizePackageImports: ["lucide-react", "lodash"],
+  }
 };
 
 export default nextConfig;
